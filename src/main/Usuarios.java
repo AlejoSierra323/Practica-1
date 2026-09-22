ac4518da29dcc29ed0c2428c1cb576a5748c33c4
nerea <25dm.nerea.fresnedo@arangoya.net>
2026-09-22 13:45:01 +0200

    Añadir datos clase Usuario

--- src/main/Usuarios.java
+++ src/main/Usuarios.java
@@ -2,4 +2,40 @@ package main;
 
 public class Usuarios {
 
+	private int id;
+	private String nombre;
+	private String correoElectronico;
+	
+	
+	public Usuarios(int id, String nombre, String correoElectronico) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.correoElectronico = correoElectronico;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	
 }
