6e839646fee9c77e2b8b6015b5fd715737b7a6c2
Iker Albizu <ikeralbizu16@gmail.com>
2026-09-22 13:36:15 +0200

    Creacción atributos Recursos -Iker

--- src/main/Recurso.java
+++ src/main/Recurso.java
@@ -1,8 +1,11 @@
 package main;
 
-public class Recurso {
+public abstract class Recurso {
 	
-	
+	protected int id;
+	protected String nombre;
+	protected int año;
+	protected boolean disponible;
 	
 	
 	
