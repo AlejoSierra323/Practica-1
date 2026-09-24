7884c8c67e705befe99c5143bf8cf3d047df367b
Iker Albizu <ikeralbizu16@gmail.com>
2026-09-24 11:09:01 +0200

    Creeación de dos metodos comentados a falta de cambios- Iker

--- src/main/Biblioteca.java
+++ src/main/Biblioteca.java
@@ -7,12 +7,29 @@ public class Biblioteca {
 	
 	private List<Usuarios> usuarios;
 	private List<Recurso> recursos;
-	private List<Prestamo> prestamos;
+	private List<Prestamo> prestamos; 
 	
 	public Biblioteca() {
 		usuarios = new ArrayList<Usuarios>();
 		recursos = new ArrayList<Recurso>();
 		prestamos = new ArrayList<Prestamo>();
 	}
-
+	
+	//public void listarRecursosDisponibles() {
+		//System.out.println("   Recursos Disponibles   ");
+		//for (Recurso rec : recursos) {
+			//if (rec.isDisponible()) {
+				//System.out.println(rec);
+			//}
+		//}
+	//}
+	
+	//public void listarRecursosPrestados() {
+		//System.out.println("   Recursos prestados   ");
+		//for (Recurso re : recursos) {
+			//if (!re.isDisponible()) {
+				//System.out.println(re);
+			//}
+		//}
+	//}
 }
