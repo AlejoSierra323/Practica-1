7cfab376dd97157e971071421a94d7b31eb2a447
Iker Albizu <ikeralbizu16@gmail.com>
2026-09-24 10:48:57 +0200

    Creeación de las Listas de los Atributos -Iker

--- src/main/Biblioteca.java
+++ src/main/Biblioteca.java
@@ -1,5 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+	
+	private List<Usuarios> usuarios;
+	private List<Recurso> recursos;
+	private List<Prestamo> prestamos;
+	
+	public Biblioteca() {
+		usuarios = new ArrayList<Usuarios>();
+		recursos = new ArrayList<Recurso>();
+		prestamos = new ArrayList<Prestamo>();
+	}
 
 }
