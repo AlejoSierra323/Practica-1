1dcf277608483ae8519ad897b8c74f912f4f9c79
Alejandro Sierra <26dm.alejandro.sierra@arangoya.net>
2026-09-25 12:54:02 +0200

    Corregido errores clase prestamo

--- src/main/Recurso.java
+++ src/main/Recurso.java
@@ -3,14 +3,46 @@ package main;
 public abstract class Recurso {
 
 	protected int id;
-	protected String nombre;
+	protected String titulo;
 	protected int año;
 	protected boolean disponible;
 
-	public Recurso(int id, String nombre, int año, boolean disponible) {
+	public Recurso(int id, String titulo, int año, boolean disponible) {
 		this.id = id;
-		this.nombre = nombre;
+		this.titulo = titulo;
 		this.año = año;
+		this.disponible = disponible;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String nombre) {
+		this.titulo = nombre;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
 		this.disponible = disponible;
 	}
 
